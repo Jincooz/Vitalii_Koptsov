a7a9aebd5133e5d5b4c75888fe8425d7c7a707ba
Vitalii <shaddyreaper@gmail.com>
2022-10-07 23:06:39 +0300

    Task7 comlpete

---
+++ Project/Tasks/Task7.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    public class Task7
+    {
+        public static string GetIPFromUInt32(uint numIp)
+        {
+            string[] segments = new string[4];
+            for (int i = 0; i < 4; i++)
+            {
+                int segmentNumber = (int)(numIp / Math.Pow(256, i) % 256);
+                segments[3-i] = segmentNumber.ToString();
+            }
+            return String.Join('.', segments);
+        }
+    }
+}
---
+++ Project/TestProject/Task7Tests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    internal class Task7Tests
+    {
+        [TestCase(2149583361, "128.32.10.1")]
+        [TestCase((uint)32, "0.0.0.32")]
+        [TestCase((uint)0, "0.0.0.0")]
+        public void GetIPFromUInt32(uint ipNum, string expectedResult)
+        {
+            var result = Tasks.Task7.GetIPFromUInt32(ipNum);
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
