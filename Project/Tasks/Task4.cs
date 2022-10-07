732d459a2fce774f4938aa63432b1e330b0295c3
Vitalii <shaddyreaper@gmail.com>
2022-10-07 20:42:21 +0300

    Task4 complete

---
+++ Project/Tasks/Task4.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    public class Task4
+    {
+        public static int CountPairsWithSum5(int[] ints)
+        {
+            int sum = 0;
+            Dictionary<int, int> pairs = new Dictionary<int, int>();
+            foreach (int i in ints)
+            {
+                if(pairs.ContainsKey(i))
+                {
+                    sum++;
+                }
+                if (pairs.ContainsKey(5 - i))
+                {
+                    pairs[5 - i]++;
+                }
+                else
+                {
+                    pairs.Add(5 - i, 0);
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ Project/TestProject/Task4Tests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    internal class Task4Tests
+    {
+        [TestCase(new int[] { 1, 2, 6, 9 })]
+        [TestCase(new int[] { })]
+        public void CountPairsWithSum5_NoSumEqualsFive_ReturnsZero(int[] ints)
+        {
+            var result = Tasks.Task4.CountPairsWithSum5(ints);
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [TestCase(new int[] { 1, 2, 3, 4 }, 2)]
+        [TestCase(new int[] { 1, 4, 6, 8 }, 1)]
+        public void CountPairsWithSum5_WithSumsEqualsFive_ReturnsAmountOfPairs(int[] ints, int expectedResult)
+        {
+            var result = Tasks.Task4.CountPairsWithSum5(ints);
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [Test]
+        public void CountPairsWithSum5_WithManyAmountsOfSameDigitWhichHavePair_ReturnsAmountOfPairsWhereThisDigitsCountsIndependetly()
+        {
+            var ints = new int[] { 1, 3, 2, 3, 3 };
+            var result = Tasks.Task4.CountPairsWithSum5(ints);
+            Assert.That(result, Is.EqualTo(3));
+        }
+    }
+}
