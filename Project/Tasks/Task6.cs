7dee71b1fd73ffe79d5c574192eea5ea49c8e15a
Vitalii <shaddyreaper@gmail.com>
2022-10-07 22:24:36 +0300

    Task6 complete

---
+++ Project/Tasks/Task6.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    public class Task6
+    {
+        public static int NextBigger(int number)
+        {
+            int amountOfDigit = (int)Math.Ceiling(Math.Log10(number));
+            for (int i = 0; i < amountOfDigit; i++)
+            {
+                int curentValue = number / (int)Math.Pow(10, i) % 10;
+                for (int j = i; j < amountOfDigit; j++)
+                {
+                    int innerValue = number / (int)Math.Pow(10, j) % 10;
+                    if (curentValue > innerValue)
+                    {
+                        return number + (curentValue - innerValue) * ((int)Math.Pow(10, j) - (int)Math.Pow(10, i)); 
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ Project/TestProject/Task6Tests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    internal class Task6Tests
+    {
+        [TestCase(12, 21)]
+        [TestCase(513, 531)]
+        [TestCase(2017, 2071)]
+        public void NextBigger_SomeNumberWhithHaveBiger_ReturnsNextBigger(int number, int expectedResult)
+        {
+            var result = Tasks.Task6.NextBigger(number);
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+
+        [TestCase(0)]
+        [TestCase(9)]
+        [TestCase(111)]
+        [TestCase(531)]
+        public void NextBigger_SomeNumberWhithoutBiger_ReturnsNegativeOne(int number)
+        {
+            var result = Tasks.Task6.NextBigger(number);
+            Assert.That(result, Is.EqualTo(-1));
+        }
+    }
+}
