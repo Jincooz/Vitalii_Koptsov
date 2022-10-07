5a2c3c52570652340caf280723c53e49b8eeafa1
Vitalii <shaddyreaper@gmail.com>
2022-10-07 22:04:26 +0300

    Task5 complete

---
+++ Project/Tasks/Task5.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    public class Task5
+    {
+        public static string DealWithFriendList(string friendList)
+        {
+            string[] friendsNames = friendList.Split(';');
+            List<Friend> friends = new List<Friend>();
+            foreach (string friendNames in friendsNames)
+            {
+                string[] names = friendNames.Split(':');
+                try
+                {
+                    friends.Add(new Friend()
+                    {
+                        FirstName = names[0],
+                        LastName = names[1]
+                    });
+                }
+                catch (Exception)
+                {
+                    throw new Exception("Friend list have unsupported format");
+                }                    
+
+            }
+            var resultList = friends.OrderBy(friend => friend.LastName)
+                                    .ThenBy(friend => friend.FirstName)
+                                    .Select(friend => friend.ToString());
+            return String.Join("" , resultList);
+        }
+    }
+    internal class Friend
+    {
+        private string _firstName = "";
+        private string _lastName = "";
+        public string FirstName { get => _firstName; set => _firstName = value.ToUpper(); }
+        public string LastName { get => _lastName; set => _lastName = value.ToUpper(); }
+        public override string ToString() 
+        {
+            return $"({LastName}, {FirstName})";
+        }
+    }
+}
---
+++ Project/TestProject/Task5Tests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    internal class Task5Tests
+    {
+        [Test]
+        public void DealWithFriendList_DenFriendList_ReturnsRequiredOutput()
+        {
+            var s = "Fred:Corwill;Wilfred:Corwill;Barney:TornBull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill";
+            var requiredResult = "(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)";
+            string result = Tasks.Task5.DealWithFriendList(s);
+            Assert.That(result, Is.EqualTo(requiredResult));
+        }
+
+        [Test]
+        public void DealWithFriendList_EmptyString_ThrowsException()
+        {
+            var s = "";
+            TestDelegate action = () => Tasks.Task5.DealWithFriendList(s);
+            Assert.Throws<Exception>(action);
+        }
+    }
+}
