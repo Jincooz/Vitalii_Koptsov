e9b23e91e17e21142d302525bdeed7d9af3791d6
Vitalii <shaddyreaper@gmail.com>
2022-10-07 19:39:58 +0300

    Task2 complete

---
+++ Project/Tasks/Task2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    public class Task2
+    {
+        public static char? FirstNonRepeatingLetter(string str)
+        {
+            Dictionary<char, int> countChars = new Dictionary<char, int>();
+            foreach (char symbol in str)
+            {
+                if (countChars.ContainsKey(Char.ToLower(symbol)))
+                {
+                    countChars[Char.ToLower(symbol)]++;
+                }
+                else if (countChars.ContainsKey(Char.ToUpper(symbol)))
+                {
+                    countChars[Char.ToUpper(symbol)]++;
+                }
+                else
+                {
+                    countChars.Add(symbol, 1);
+                }
+            }
+            foreach (var item in countChars)
+            {
+                if (item.Value == 1)
+                {
+                    return item.Key;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ Project/TestProject/Task2Tests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    internal class Task2Tests
+    {
+        [Test]
+        public void FirstNonRepeatingLetter_String_ReturnFirstNonRepeatingLetterInThisString()
+        {
+            var input = "sTreSS";
+            var result = Tasks.Task2.FirstNonRepeatingLetter(input);
+            Assert.That(result, Is.EqualTo('T'));
+        }
+
+        [Test]
+        public void FirstNonRepeatingLetter_EmptyString_ReturnsNull()
+        {
+            var input = "";
+            var result = Tasks.Task2.FirstNonRepeatingLetter(input);
+
+            Assert.That(result, Is.EqualTo(null));
+        }
+
+        [TestCase("aa")]
+        [TestCase("bbaa")]
+        public void FirstNonRepeatingLetter_StringWithRepeatingLettersOnly_ReturnsNull(string inputWithRepeatingLettersOnly)
+        {
+            var result = Tasks.Task2.FirstNonRepeatingLetter(inputWithRepeatingLettersOnly);
+            Assert.That(result, Is.EqualTo(null));
+        }
+    }
+}
