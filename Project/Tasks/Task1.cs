700a7c352b53715ff9ffbe81974dccfc0d23147e
Vitalii <shaddyreaper@gmail.com>
2022-10-07 19:38:20 +0300

    Lab1 complete

---
+++ Project/Tasks/Task1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    public class Task1
+    {
+        public static List<int> GetIntegersFromList(List<object> innputList)
+        {
+            return innputList.Where(element => element is int)
+                             .Select(element => (int)element)
+                             .ToList();
+        }
+    }
+}
---
+++ Project/TestProject/Task1Tests.cs
@@ -0,0 +1,21 @@
+namespace ProjectTests
+{
+    public class Task1Tests
+    {
+        [Test]
+        public void GetIntegersFromList_ListWithTwoIntsAndOtherTypes_ReturnsListWithSameInts()
+        {
+            var List = new List<object>() {1, 'a', 'b', "aasf", '1', "123", 2, 12123.02,};
+            var result = Tasks.Task1.GetIntegersFromList(List);
+            Assert.That(result, Is.EqualTo(new List<int> { 1, 2}));
+        }
+
+        [Test]
+        public void GetIntegersFromList_OnlyStringList_ReturnsEmptyList()
+        {
+            var List = new List<object>() {'a'};
+            var result = Tasks.Task1.GetIntegersFromList(List);
+            Assert.That(result, Is.EqualTo(new List<int> {}));
+        }
+    }
+}
