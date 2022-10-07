af2caef44af5a7ecf7c3131716b80c381153bd15
Vitalii <shaddyreaper@gmail.com>
2022-10-07 19:57:25 +0300

    Task3 complete

---
+++ Project/Tasks/Task3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    public class Task3
+    {
+        public static int DigitalRoot(int number)
+        {
+            int result = 0;
+            while(number > 0)
+            {
+                result += number % 10;
+                number /= 10;
+            }
+            if (result > 10)
+                return DigitalRoot(result);
+            else
+                return result;
+        }
+    }
+}
---
+++ Project/TestProject/Task3Tests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    internal class Task3Tests
+    {
+        [Test]
+        public void DigitalRoot_NumberThatLowerThen10_ResturnsSameNumber()
+        {
+            int number = 5;
+            var result = Tasks.Task3.DigitalRoot(number);
+            Assert.That(result, Is.EqualTo(number));
+        }
+
+        [TestCase(100, 1)]
+        [TestCase(123, 6)]
+        [TestCase(155, 2)]
+        [TestCase(2412455, 5)]
+        public void DigitalRoot_NumberThatBiggerThen10_ResturnsDigitalRootOfThisNumber(int number, int expectedResult)
+        {
+            var result = Tasks.Task3.DigitalRoot(number);
+            Assert.That(result, Is.EqualTo(expectedResult));
+        }
+    }
+}
