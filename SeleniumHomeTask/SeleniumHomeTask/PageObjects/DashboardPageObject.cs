ebc4d21ac3f4b6b1104d9b9c03f9e13731a39ba0
Vitalii <shaddyreaper@gmail.com>
2022-12-04 10:40:38 +0200

    Add DashboardPageObject

---
+++ SeleniumHomeTask/SeleniumHomeTask/PageObjects/DashboardPageObject.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumHomeTask.PageObjects
+{
+    internal class DashboardPageObject : PageObject
+    {
+        private static readonly string _pageUrl = "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index";
+        private IWebElement AdminMenuItem => _webDriver.FindElement(By.CssSelector("a.oxd-main-menu-item"));
+        public DashboardPageObject(IWebDriver webDriver) : base(webDriver, _pageUrl) { }
+        public SystemUsersPageObject GoToAdminPage()
+        {
+            AdminMenuItem.Click();
+            return new SystemUsersPageObject(_webDriver);
+        }
+    }
+}
