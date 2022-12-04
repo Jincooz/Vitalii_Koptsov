79d3dedf4e02a2e6e23d993d4bde102ae32b28e0
Vitalii <shaddyreaper@gmail.com>
2022-12-04 13:55:51 +0200

    Add SystemUsersPageObject

---
+++ SeleniumHomeTask/SeleniumHomeTask/PageObjects/AdminJobDropDownMenuPageComponent.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumHomeTask.PageObjects
+{
+    internal class AdminJobDropDownMenuPageComponent : PageComponent
+    {
+        private IWebElement JobTitlesReference => FindElement(By.ClassName("oxd-topbar-body-nav-tab-link"));
+        public AdminJobDropDownMenuPageComponent(IWebElement webElement) : base(webElement) { }
+        public void OpenJobTitleList()
+        {
+            JobTitlesReference.Click();
+        }
+    }
+}
---
+++ SeleniumHomeTask/SeleniumHomeTask/PageObjects/SystemUsersPageObject.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumHomeTask.PageObjects
+{
+    internal class SystemUsersPageObject : PageObject
+    {
+        private static readonly string _pageUrl = "https://opensource-demo.orangehrmlive.com/web/index.php/admin/viewSystemUsers";
+        private IWebElement JobTitle => FindElements(By.ClassName("oxd-topbar-body-nav-tab"))[1];
+        private AdminJobDropDownMenuPageComponent? JobMenu;
+        public SystemUsersPageObject(IWebDriver webDriver) : base(webDriver, _pageUrl) { }
+        public JobTitleListPageObject OpenJobTitleList()
+        {
+            JobTitle.Click();
+            InitializeJobMenu();
+            JobMenu.OpenJobTitleList();
+            return new JobTitleListPageObject(_webDriver);
+        }
+        private void InitializeJobMenu()
+        {
+            if (JobMenu != null)
+            {
+                JobMenu = new AdminJobDropDownMenuPageComponent(FindElement(By.XPath("//*[@id=\"app\"]/div[1]/div[1]/header/div[2]/nav/ul/li[2]/ul")));
+            }
+        }
+    }
+}
+
