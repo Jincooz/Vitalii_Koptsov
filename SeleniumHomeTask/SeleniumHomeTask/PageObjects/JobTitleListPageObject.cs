31e6104b0e90ee3272a88aead1be478743f05c6b
Vitalii <shaddyreaper@gmail.com>
2022-12-04 14:02:56 +0200

    Add JobTitleListPageObject

---
+++ SeleniumHomeTask/SeleniumHomeTask/PageObjects/JobTitleListPageObject.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumHomeTask.PageObjects
+{
+    internal class JobTitleListPageObject : PageObject
+    {
+        private static readonly string _pageUrl = "https://opensource-demo.orangehrmlive.com/web/index.php/admin/viewJobTitleList";
+        private IWebElement AddButton => FindElement(By.ClassName("oxd-button"));
+        public JobTitleListPageObject(IWebDriver webDriver) : base(webDriver, _pageUrl) { }
+        public SaveJobTitlePageObject ClickAdd()
+        {
+            AddButton.Click();
+            return new SaveJobTitlePageObject(_webDriver);
+        }
+    }
+}
