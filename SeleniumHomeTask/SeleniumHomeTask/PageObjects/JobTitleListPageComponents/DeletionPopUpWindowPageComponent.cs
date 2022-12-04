b1052b577009f42f46179bb22778d3fab83885b1
Vitalii <shaddyreaper@gmail.com>
2022-12-04 14:34:13 +0200

    Add table and deletion button interactions to JobTitleListPageObject

---
+++ SeleniumHomeTask/SeleniumHomeTask/PageObjects/JobTitleListPageComponents/DeletionPopUpWindowPageComponent.cs
@@ -0,0 +1,12 @@
+﻿namespace SeleniumHomeTask.PageObjects.JobTitleListPageObjectr
+{
+    internal class DeletionPopUpWindowPageComponent : PageComponent
+    {
+        private IWebElement DeleteButton => FindElement(By.ClassName("oxd-button--label-danger"));
+        public DeletionPopUpWindowPageComponent(IWebElement popUpWindow) : base(popUpWindow) { }
+        public void ClickDelete()
+        {
+            DeleteButton.Click();
+        }
+    }
+}
--- SeleniumHomeTask/SeleniumHomeTask/PageObjects/JobTitleListPageObject.cs
+++ SeleniumHomeTask/SeleniumHomeTask/PageObjects/JobTitleListPageObject.cs
@@ -11,11 +11,40 @@ namespace SeleniumHomeTask.PageObjects
     {
         private static readonly string _pageUrl = "https://opensource-demo.orangehrmlive.com/web/index.php/admin/viewJobTitleList";
         private IWebElement AddButton => FindElement(By.ClassName("oxd-button"));
+        private IWebElement DeleteSelectedButton => FindElement(By.ClassName("oxd-button--label-danger"));
+        private IWebElement PopUpWindow => FindElement(By.ClassName("orangehrm-dialog-popup"));
+        private JobTitleListPageObjectr.JobTablePageComponent? JobTable;
         public JobTitleListPageObject(IWebDriver webDriver) : base(webDriver, _pageUrl) { }
         public SaveJobTitlePageObject ClickAdd()
         {
             AddButton.Click();
             return new SaveJobTitlePageObject(_webDriver);
+        }
+        public List<string> GetListOfJobTitlesInTable()
+        {
+            InitializeJobTable();
+            return JobTable.GetListOfJobTitles();
+        }
+        public string GetJobDesctitionByJobTitle(string jobTitle)
+        {
+            InitializeJobTable();
+            return JobTable.GetJobDesctitionByJobTitle(jobTitle);
+        }
+        public void CheckCheckboxWithJobTitle(string title)
+        {
+            InitializeJobTable();
+            JobTable.CheckCheckboxWithJobTitle(title);
+        }
+        public JobTitleListPageObject DeleteSelected()
+        {
+            DeleteSelectedButton.Click();
+            new JobTitleListPageObjectr.DeletionPopUpWindowPageComponent(PopUpWindow).ClickDelete();
+            return new JobTitleListPageObject(_webDriver);
+        }
+        private void InitializeJobTable()
+        {
+            if (JobTable == null)
+                JobTable = new(FindElement(By.ClassName("oxd-table")));
         }
     }
 }
