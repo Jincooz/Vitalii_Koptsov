e4160306a5e840cdfacdbc593fcf344b6b8c8fc4
Vitalii <shaddyreaper@gmail.com>
2022-12-04 14:32:01 +0200

    Add SaveJobTitlePageObject

---
+++ SeleniumHomeTask/SeleniumHomeTask/PageObjects/SaveJobTitlePageObject.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumHomeTask.PageObjects
+{
+    internal class SaveJobTitlePageObject : PageObject
+    {
+        private static readonly string _pageUrl = "https://opensource-demo.orangehrmlive.com/web/index.php/admin/saveJobTitle";
+        private IWebElement JobTitleTextBox => FindElement(By.XPath("//*[@id=\"app\"]/div[1]/div[2]/div[2]/div/div/form/div[1]/div/div[2]/input"));
+        private IWebElement JobDescriptionTextBox => FindElements(By.ClassName("oxd-textarea"))[0];
+        private IWebElement NoteTextBox => FindElements(By.ClassName("oxd-textarea"))[1];
+        private IWebElement SaveButton => FindElement(By.ClassName("orangehrm-left-space"));
+        public SaveJobTitlePageObject(IWebDriver webDriver) : base(webDriver, _pageUrl) { }
+        public void EnterJobTitle(string title)
+        {
+            JobTitleTextBox.Clear();
+            JobTitleTextBox.SendKeys(title);
+        }
+        public void EnterJobDescription(string description)
+        {
+            JobDescriptionTextBox.Clear();
+            JobDescriptionTextBox.SendKeys(description);
+        }
+        public void EnterJobNote(string note)
+        {
+            NoteTextBox.Clear();
+            NoteTextBox.SendKeys(note);
+        }
+        public JobTitleListPageObject ClickSave()
+        {
+            SaveButton.Click();
+            return new JobTitleListPageObject(_webDriver);
+        }
+    }
+}
