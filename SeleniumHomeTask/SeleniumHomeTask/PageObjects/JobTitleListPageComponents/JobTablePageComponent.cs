33a193f90e8e679da18f1f7d5e94671b809061d4
Vitalii <shaddyreaper@gmail.com>
2022-12-04 14:33:14 +0200

    Add Job Table component

---
+++ SeleniumHomeTask/SeleniumHomeTask/PageObjects/JobTitleListPageComponents/JobTablePageComponent.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+
+namespace SeleniumHomeTask.PageObjects.JobTitleListPageObjectr
+{
+    internal class JobTablePageComponent : PageComponent
+    {
+        [OneTimeSetUp]
+        public void Init()
+        {
+            Environment.CurrentDirectory = Path.GetDirectoryName(GetType().Assembly.Location);
+        }
+        private List<JobTableRowPageComponent> TableRows { get; set; }
+        public JobTablePageComponent(IWebElement webElement) : base(webElement)
+        {
+            TableRows = new();
+            var listOfRows = FindElements(By.ClassName("oxd-table-row"));
+            for (int i = 1; i < listOfRows.Count; i++)
+            {
+                TableRows.Add(new JobTableRowPageComponent(listOfRows[i]));
+            }
+        }
+        public List<string> GetListOfJobTitles()
+        {
+            return TableRows.Select(x => x.JobTitle).ToList();
+        }
+        public string GetJobDesctitionByJobTitle(string jobTitle)
+        {
+            return TableRows.Find(e => e.JobTitle == jobTitle).JobDescription;
+        }
+        public void CheckCheckboxWithJobTitle(string title)
+        {
+            TableRows.Find(e => e.JobTitle == title).Check();
+        }
+    }
+}
---
+++ SeleniumHomeTask/SeleniumHomeTask/PageObjects/JobTitleListPageComponents/JobTableRowPageComponent.cs
@@ -0,0 +1,34 @@
+﻿namespace SeleniumHomeTask.PageObjects.JobTitleListPageObjectr
+{
+    internal class JobTableRowPageComponent : PageComponent
+    {
+        public IWebElement CheckBox => FindElement(By.XPath("div[1]/div"));
+        public string JobTitle { get; }
+        public string JobDescription { get; }
+        public IWebElement DeleteButton => FindElements(By.ClassName("oxd-icon-button"))[0];
+        public IWebElement EditButton => FindElements(By.ClassName("oxd-icon-button"))[1];
+        public JobTableRowPageComponent(IWebElement row) : base(row)
+        {
+            JobTitle = FindElement(By.XPath("div[2]/div")).Text;
+            JobDescription = FindElement(By.XPath("div[3]/div")).Text;
+        }
+        public bool IsChecked()
+        {
+            return CheckBox.Selected;
+        }
+        public void Check()
+        {
+            if(!IsChecked())
+            {
+                CheckBox.Click();
+            }
+        }
+        public void Uncheck() 
+        {
+            if(IsChecked()) 
+            { 
+                CheckBox.Click();
+            }        
+        }
+    }
+}
