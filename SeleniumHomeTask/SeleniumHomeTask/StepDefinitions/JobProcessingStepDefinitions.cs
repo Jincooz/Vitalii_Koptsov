7f2815ca1aa379df2a2a1f2ceda5d60ac52033c3
Vitalii <shaddyreaper@gmail.com>
2022-12-04 10:01:16 +0200

    Add step defenition file for JobProcessing.feature Delete example CalculatorSpepDefenitions.cs

---
+++ SeleniumHomeTask/SeleniumHomeTask/StepDefinitions/JobProcessingStepDefinitions.cs
@@ -0,0 +1,87 @@
+using System;
+using TechTalk.SpecFlow;
+
+namespace SeleniumHomeTask.StepDefinitions
+{
+    [Binding]
+    public class JobProcessingStepDefinitions
+    {
+        [Given(@"a admin on the admin viewJobTitleList page")]
+        public void GivenAAdminOnTheAdminViewJobTitleListPage()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"the admin click on Add button")]
+        public void GivenTheAdminClickOnAddButton()
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"the admin enters '([^']*)' into Job Title field")]
+        public void GivenTheAdminEntersIntoJobTitleField(string p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"the admin enters '([^']*)' into Job Description field")]
+        public void GivenTheAdminEntersIntoJobDescriptionField(string p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"the admin enters '([^']*)' into Note field")]
+        public void GivenTheAdminEntersIntoNoteField(string p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"the admin click on Save button")]
+        public void WhenTheAdminClickOnSaveButton()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"the admin is redirected to the admin viewJobTitleList page")]
+        public void ThenTheAdminIsRedirectedToTheAdminViewJobTitleListPage()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"row with '([^']*)' as Job Title and with '([^']*)' as Job Description exist in table")]
+        public void ThenRowWithAsJobTitleAndWithAsJobDescriptionExistInTable(string p0, string p1)
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"Job Title '([^']*)' exist in table")]
+        public void GivenJobTitleExistInTable(string p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"the admin checks checkbox coresponding to '([^']*)' Job Title")]
+        public void GivenTheAdminChecksCheckboxCorespondingToJobTitle(string p0)
+        {
+            throw new PendingStepException();
+        }
+
+        [Given(@"the admin scrolls up to top")]
+        public void GivenTheAdminScrollsUpToTop()
+        {
+            throw new PendingStepException();
+        }
+
+        [When(@"the admin clicks Delete Selected and ensure it in pop up window")]
+        public void WhenTheAdminClicksDeleteSelectedAndEnsureItInPopUpWindow()
+        {
+            throw new PendingStepException();
+        }
+
+        [Then(@"row with '([^']*)' as Job Title don`t exist in table")]
+        public void ThenRowWithAsJobTitleDonTExistInTable(string p0)
+        {
+            throw new PendingStepException();
+        }
+    }
+}
