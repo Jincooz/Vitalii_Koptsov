cce519c35192472582d5bc34678ad3f5730a36e8
Vitalii <shaddyreaper@gmail.com>
2022-12-04 10:17:11 +0200

    Create PageObject class And PageObjects folder

---
+++ SeleniumHomeTask/SeleniumHomeTask/PageObjects/PageObject.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumHomeTask.PageObjects
+{
+    internal abstract class PageObject
+    {
+        protected readonly IWebDriver _webDriver;
+        private string _pageUrl;
+        public PageObject(IWebDriver webDriver, string pageUrl)
+        {
+            _pageUrl = pageUrl;
+            _webDriver = webDriver;
+        }
+        public PageObject GoTo()
+        {
+            _webDriver.Navigate().GoToUrl(_pageUrl);
+            return this;
+        }
+        public bool isValidToPageObject => _webDriver.Url != _pageUrl;
+        public void Close()
+        {
+            _webDriver.Quit();
+        }
+    }
+}
