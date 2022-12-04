16255e49ccf093baf2b4177d54101298096b5b92
Vitalii <shaddyreaper@gmail.com>
2022-12-04 15:17:26 +0200

    Add step defenition for JobProcessing.feature

--- SeleniumHomeTask/SeleniumHomeTask/StepDefinitions/JobProcessingStepDefinitions.cs
+++ SeleniumHomeTask/SeleniumHomeTask/StepDefinitions/JobProcessingStepDefinitions.cs
@@ -1,87 +1,109 @@
-using System;
-using TechTalk.SpecFlow;
+using NUnit.Framework;
+using SeleniumHomeTask.PageObjects;
 
 namespace SeleniumHomeTask.StepDefinitions
 {
     [Binding]
-    public class JobProcessingStepDefinitions
+    public sealed class JobProcessingStepDefinitions
     {
+        private PageObjects.PageObject? currentPage;
+        [OneTimeSetUp]
+        public void Init()
+        {
+            Environment.CurrentDirectory = Path.GetDirectoryName(GetType().Assembly.Location);
+        }
         [Given(@"a admin on the admin viewJobTitleList page")]
         public void GivenAAdminOnTheAdminViewJobTitleListPage()
         {
-            throw new PendingStepException();
+            currentPage = MbFactory.getJobTitleListPageObject(MbFactory.getDriver());
         }
 
         [Given(@"the admin click on Add button")]
         public void GivenTheAdminClickOnAddButton()
         {
-            throw new PendingStepException();
+            currentPage = (currentPage as JobTitleListPageObject).ClickAdd();
         }
 
         [Given(@"the admin enters '([^']*)' into Job Title field")]
-        public void GivenTheAdminEntersIntoJobTitleField(string p0)
+        public void GivenTheAdminEntersIntoJobTitleField(string jobTitle)
         {
-            throw new PendingStepException();
+            (currentPage as SaveJobTitlePageObject).EnterJobTitle(jobTitle);
         }
 
         [Given(@"the admin enters '([^']*)' into Job Description field")]
-        public void GivenTheAdminEntersIntoJobDescriptionField(string p0)
+        public void GivenTheAdminEntersIntoJobDescriptionField(string jobDescription)
         {
-            throw new PendingStepException();
+            (currentPage as SaveJobTitlePageObject).EnterJobDescription(jobDescription);
         }
 
         [Given(@"the admin enters '([^']*)' into Note field")]
-        public void GivenTheAdminEntersIntoNoteField(string p0)
+        public void GivenTheAdminEntersIntoNoteField(string jobNote)
         {
-            throw new PendingStepException();
+            (currentPage as SaveJobTitlePageObject).EnterJobNote(jobNote);
         }
 
         [When(@"the admin click on Save button")]
         public void WhenTheAdminClickOnSaveButton()
         {
-            throw new PendingStepException();
+            currentPage = (currentPage as SaveJobTitlePageObject).ClickSave();
         }
 
         [Then(@"the admin is redirected to the admin viewJobTitleList page")]
         public void ThenTheAdminIsRedirectedToTheAdminViewJobTitleListPage()
         {
-            throw new PendingStepException();
+            Assert.That(currentPage.isValidToPageObject);
         }
 
         [Then(@"row with '([^']*)' as Job Title and with '([^']*)' as Job Description exist in table")]
-        public void ThenRowWithAsJobTitleAndWithAsJobDescriptionExistInTable(string p0, string p1)
+        public void ThenRowWithAsJobTitleAndWithAsJobDescriptionExistInTable(string jobTitle, string jobDescription)
         {
-            throw new PendingStepException();
+            List<string> jobTitles = (currentPage as JobTitleListPageObject).GetListOfJobTitlesInTable();
+            Assert.Contains(jobTitle, jobTitles);
+            string jobDescriptionOnSite = (currentPage as JobTitleListPageObject).GetJobDesctitionByJobTitle(jobTitle);
+            Assert.That(jobDescription.Contains(jobDescriptionOnSite.TrimStart()));
+            currentPage.Close();
         }
-
-        [Given(@"Job Title '([^']*)' exist in table")]
-        public void GivenJobTitleExistInTable(string p0)
+        public void CreateJobWithJobTitle(string jobTitle)
         {
-            throw new PendingStepException();
+            GivenAAdminOnTheAdminViewJobTitleListPage();
+            GivenTheAdminClickOnAddButton();
+            GivenTheAdminEntersIntoJobTitleField(jobTitle);
+            WhenTheAdminClickOnSaveButton();
+            currentPage.Close();
+        }
+        [Given(@"Job Title '([^']*)' exist in table")]
+        public void GivenJobTitleExistInTable(string jobTitle)
+        {
+            if (!(currentPage as JobTitleListPageObject).GetListOfJobTitlesInTable().Contains(jobTitle))
+            {
+                CreateJobWithJobTitle(jobTitle);
+            }
         }
 
         [Given(@"the admin checks checkbox coresponding to '([^']*)' Job Title")]
-        public void GivenTheAdminChecksCheckboxCorespondingToJobTitle(string p0)
+        public void GivenTheAdminChecksCheckboxCorespondingToJobTitle(string jobTitle)
         {
-            throw new PendingStepException();
+            (currentPage as JobTitleListPageObject).CheckCheckboxWithJobTitle(jobTitle);
         }
 
         [Given(@"the admin scrolls up to top")]
         public void GivenTheAdminScrollsUpToTop()
         {
-            throw new PendingStepException();
+            currentPage.ScrollToTop();
         }
 
         [When(@"the admin clicks Delete Selected and ensure it in pop up window")]
-        public void WhenTheAdminClicksDeleteSelectedAndEnsureItInPopUpWindow()
+        public void WhenTheAdminClicksDeleteSelected()
         {
-            throw new PendingStepException();
+            currentPage = (currentPage as JobTitleListPageObject).DeleteSelected();
         }
 
         [Then(@"row with '([^']*)' as Job Title don`t exist in table")]
-        public void ThenRowWithAsJobTitleDonTExistInTable(string p0)
+        public void ThenRowWithAsJobTitleDonTExistInTable(string jobTitle)
         {
-            throw new PendingStepException();
+            List<string> jobTitles = (currentPage as JobTitleListPageObject).GetListOfJobTitlesInTable();
+            Assert.That(!jobTitles.Contains(jobTitle));
+            currentPage.Close();
         }
     }
 }
