0ff84c3779bd92a90094f38c17a5a0b4d32c737c
Vitalii <shaddyreaper@gmail.com>
2022-12-04 15:16:36 +0200

    Add something like StaticFactory for encapsulating PageObjects creation

---
+++ SeleniumHomeTask/SeleniumHomeTask/PageObjectStaticFabric.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools;
+using SeleniumHomeTask.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumHomeTask
+{
+    internal class PageObjectStaticFabric
+    {
+        public static PageObjects.JobTitleListPageObject getJobTitleListPageObject(IWebDriver _driver )
+        {
+            return getSystemUsersPageObject(_driver).OpenJobTitleList();
+        }
+        public static PageObjects.SystemUsersPageObject getSystemUsersPageObject(IWebDriver _driver)
+        {
+            return getDashboardPageObject(_driver).GoToAdminPage();
+        }
+        public static PageObjects.DashboardPageObject getDashboardPageObject(IWebDriver _driver)
+        {
+            var loginPage = getLoginPageObject(_driver);
+            loginPage.EnterUsername();
+            loginPage.EnterPassword();
+            return loginPage.ClickLogin();
+        }
+        public static PageObjects.LoginPageObject getLoginPageObject(IWebDriver _driver)
+        {
+            PageObject loginPage = new LoginPageObject(_driver);
+            return (LoginPageObject)(loginPage.GoTo());
+        }
+        public static IWebDriver getDriver()
+        {
+            return new ChromeDriver();
+        }
+    }
+}
