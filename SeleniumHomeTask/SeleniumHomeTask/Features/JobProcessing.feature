9d2162c70b78b50ff8173b3da683cfb4768602c7
Vitalii <shaddyreaper@gmail.com>
2022-12-21 19:14:40 +0200

    Change feature file to more declarative style

--- SeleniumHomeTask/SeleniumHomeTask/Features/JobProcessing.feature
+++ SeleniumHomeTask/SeleniumHomeTask/Features/JobProcessing.feature
@@ -1,19 +1,21 @@
 ﻿Feature: Job processing
 
-Scenario: Add new job
+Scenario Outline: Add new job
 	Given a admin on the admin viewJobTitleList page
 	And the admin click on Add button
-	And the admin enters 'Student or Intern' into Job Title field
-	And the admin enters 'Very new, very kind, need to pass practice with us' into Job Description field
-	And the admin enters 'Need to promote them to our team if he/she good with programing' into Note field
-	When the admin click on Save button
-	Then the admin is redirected to the admin viewJobTitleList page
-	And row with 'Student or Intern' as Job Title and with 'Very new, very kind, need to pass practice with us' as Job Description exist in table
+	When the admin saves new job with valid 'job_titile', 'job_description' and 'note'
+	Then a job with 'job_titile' and with 'job_description' exist in table
 
-Scenario: Delete job
+Examples:
+ |job_titile       |job_description                                   |note                                                           |
+ |Student or Intern|Very new, very kind, need to pass practice with us|Need to promote them to our team if he/she good with programing|
+
+Scenario Outline: Delete job
 	Given a admin on the admin viewJobTitleList page
-	And Job Title 'Student or Intern' exist in table
-	And the admin checks checkbox coresponding to 'Student or Intern' Job Title
-	And the admin scrolls up to top
-	When the admin clicks Delete Selected and ensure it in pop up window
-	Then row with 'Student or Intern' as Job Title don`t exist in table
+	And a job with 'job_titile' exist in table
+	When the admin delete a job with 'job_title' through Delete Selected button 
+	Then row with 'job_titile' don`t exist in table
+
+Examples: 
+ |job_title        |
+ |Student or Intern|
--- SeleniumHomeTask/SeleniumHomeTask/StepDefinitions/JobProcessingStepDefinitions.cs
+++ SeleniumHomeTask/SeleniumHomeTask/StepDefinitions/JobProcessingStepDefinitions.cs
@@ -24,38 +24,18 @@ namespace SeleniumHomeTask.StepDefinitions
             currentPage = (currentPage as JobTitleListPageObject).ClickAdd();
         }
 
-        [Given(@"the admin enters '([^']*)' into Job Title field")]
-        public void GivenTheAdminEntersIntoJobTitleField(string jobTitle)
+        [When(@"the admin saves new job with valid '([^']*)', '([^']*)' and '([^']*)'")]
+        public void WhenTheAdminSavesNewJobWithValidAnd(string jobTitle, string jobDescription, string jobNote)
         {
-            (currentPage as SaveJobTitlePageObject).EnterJobTitle(jobTitle);
+            SaveJobTitlePageObject page = (currentPage as SaveJobTitlePageObject);
+            page.EnterJobTitle(jobTitle);
+            page.EnterJobDescription(jobDescription);
+            page.EnterJobNote(jobNote);
+            currentPage = page.ClickSave();
         }
 
-        [Given(@"the admin enters '([^']*)' into Job Description field")]
-        public void GivenTheAdminEntersIntoJobDescriptionField(string jobDescription)
-        {
-            (currentPage as SaveJobTitlePageObject).EnterJobDescription(jobDescription);
-        }
-
-        [Given(@"the admin enters '([^']*)' into Note field")]
-        public void GivenTheAdminEntersIntoNoteField(string jobNote)
-        {
-            (currentPage as SaveJobTitlePageObject).EnterJobNote(jobNote);
-        }
-
-        [When(@"the admin click on Save button")]
-        public void WhenTheAdminClickOnSaveButton()
-        {
-            currentPage = (currentPage as SaveJobTitlePageObject).ClickSave();
-        }
-
-        [Then(@"the admin is redirected to the admin viewJobTitleList page")]
-        public void ThenTheAdminIsRedirectedToTheAdminViewJobTitleListPage()
-        {
-            Assert.That(currentPage.isValidToPageObject);
-        }
-
-        [Then(@"row with '([^']*)' as Job Title and with '([^']*)' as Job Description exist in table")]
-        public void ThenRowWithAsJobTitleAndWithAsJobDescriptionExistInTable(string jobTitle, string jobDescription)
+        [Then(@"a job with '([^']*)' and with '([^']*)' exist in table")]
+        public void ThenAJobWithAndWithExistInTable(string jobTitle, string jobDescription)
         {
             List<string> jobTitles = (currentPage as JobTitleListPageObject).GetListOfJobTitlesInTable();
             Assert.Contains(jobTitle, jobTitles);
@@ -63,16 +43,16 @@ namespace SeleniumHomeTask.StepDefinitions
             Assert.That(jobDescription.Contains(jobDescriptionOnSite.TrimStart()));
             currentPage.Close();
         }
+
         public void CreateJobWithJobTitle(string jobTitle)
         {
             GivenAAdminOnTheAdminViewJobTitleListPage();
             GivenTheAdminClickOnAddButton();
-            GivenTheAdminEntersIntoJobTitleField(jobTitle);
-            WhenTheAdminClickOnSaveButton();
-            currentPage.Close();
+            WhenTheAdminSavesNewJobWithValidAnd(jobTitle, "", "");
         }
-        [Given(@"Job Title '([^']*)' exist in table")]
-        public void GivenJobTitleExistInTable(string jobTitle)
+
+        [Given(@"a job with '([^']*)' exist in table")]
+        public void GivenAJobWithExistInTable(string jobTitle)
         {
             if (!(currentPage as JobTitleListPageObject).GetListOfJobTitlesInTable().Contains(jobTitle))
             {
@@ -80,30 +60,21 @@ namespace SeleniumHomeTask.StepDefinitions
             }
         }
 
-        [Given(@"the admin checks checkbox coresponding to '([^']*)' Job Title")]
-        public void GivenTheAdminChecksCheckboxCorespondingToJobTitle(string jobTitle)
+        [When(@"the admin delete a job with '([^']*)' through Delete Selected button")]
+        public void WhenTheAdminDeleteAJobWithThroughDeleteSelectedButton(string jobTitle)
         {
             (currentPage as JobTitleListPageObject).CheckCheckboxWithJobTitle(jobTitle);
-        }
-
-        [Given(@"the admin scrolls up to top")]
-        public void GivenTheAdminScrollsUpToTop()
-        {
             currentPage.ScrollToTop();
-        }
-
-        [When(@"the admin clicks Delete Selected and ensure it in pop up window")]
-        public void WhenTheAdminClicksDeleteSelected()
-        {
             currentPage = (currentPage as JobTitleListPageObject).DeleteSelected();
         }
 
-        [Then(@"row with '([^']*)' as Job Title don`t exist in table")]
-        public void ThenRowWithAsJobTitleDonTExistInTable(string jobTitle)
+        [Then(@"row with '([^']*)' don`t exist in table")]
+        public void ThenRowWithDonTExistInTable(string jobTitle)
         {
             List<string> jobTitles = (currentPage as JobTitleListPageObject).GetListOfJobTitlesInTable();
             Assert.That(!jobTitles.Contains(jobTitle));
             currentPage.Close();
         }
+
     }
 }
