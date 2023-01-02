67a0f226ae3162a44b26711bae04c238dcc5a248
Vitalii <shaddyreaper@gmail.com>
2023-01-02 16:54:30 +0200

    Add private property for lazy loading JobMenu

--- SeleniumHomeTask/SeleniumHomeTask/PageObjects/SystemUsersPageObject.cs
+++ SeleniumHomeTask/SeleniumHomeTask/PageObjects/SystemUsersPageObject.cs
@@ -3,7 +3,16 @@
     internal class SystemUsersPageObject : PageObject
     {
         private readonly By _topMenuPath = By.ClassName("oxd-topbar-body-nav-tab");
-        private AdminJobDropDownMenuPageComponent? JobMenu;//TODO: Add private property for lazy loading JobMenu
+        private readonly By _jobMenuPath = By.XPath("//*[@id=\"app\"]/div[1]/div[1]/header/div[2]/nav/ul/li[2]/ul");
+        private AdminJobDropDownMenuPageComponent? _jobMenu;
+        private AdminJobDropDownMenuPageComponent? JobMenu
+        {
+            get
+            {
+                _jobMenu ??= new AdminJobDropDownMenuPageComponent(FindElement(_jobMenuPath));
+                return _jobMenu;
+            }
+        }
         public SystemUsersPageObject(IWebDriver webDriver) : base(webDriver)
         {
             _pageUrl = "https://opensource-demo.orangehrmlive.com/web/index.php/admin/viewSystemUsers";
@@ -11,13 +20,8 @@
         public JobTitleListPageObject OpenJobTitleList()
         {
             FindElements(_topMenuPath)[1].Click();
-            InitializeJobMenu();
             JobMenu.OpenJobTitleList();
             return new JobTitleListPageObject(_webDriver);
-        }
-        private void InitializeJobMenu()
-        {
-            JobMenu ??= new AdminJobDropDownMenuPageComponent(FindElement(By.XPath("//*[@id=\"app\"]/div[1]/div[1]/header/div[2]/nav/ul/li[2]/ul")));
         }
     }
 }
