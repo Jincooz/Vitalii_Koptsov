0b69009928bba304e6d3de9e8c31959e36c3cff4
Vitalii <shaddyreaper@gmail.com>
2023-01-02 11:36:26 +0200

    Fix .feture filem, and step definition regex

--- SeleniumHomeTask/SeleniumHomeTask/Features/JobProcessing.feature
+++ SeleniumHomeTask/SeleniumHomeTask/Features/JobProcessing.feature
@@ -3,18 +3,18 @@
 Scenario Outline: Add new job
 	Given a admin on the admin viewJobTitleList page
 	And the admin click on Add button
-	When the admin saves new job with valid 'job_titile', 'job_description' and 'note'
-	Then a job with 'job_titile' and with 'job_description' exist in table
+	When the admin saves new job with valid <job_title>, <job_description> and <note>
+	Then a job with <job_title> and with <job_description> exist in table
 
 Examples:
- |job_titile       |job_description                                   |note                                                           |
+ |job_title       |job_description                                   |note                                                           |
  |Student or Intern|Very new, very kind, need to pass practice with us|Need to promote them to our team if he/she good with programing|
 
 Scenario Outline: Delete job
 	Given a admin on the admin viewJobTitleList page
-	And a job with 'job_titile' exist in table
-	When the admin delete a job with 'job_title' through Delete Selected button 
-	Then row with 'job_titile' don`t exist in table
+	And a job with <job_title> exist in table
+	When the admin delete a job with <job_title> through Delete Selected button 
+	Then row with <job_title> don`t exist in table
 
 Examples: 
  |job_title        |
--- SeleniumHomeTask/SeleniumHomeTask/StepDefinitions/JobProcessingStepDefinitions.cs
+++ SeleniumHomeTask/SeleniumHomeTask/StepDefinitions/JobProcessingStepDefinitions.cs
@@ -29,7 +29,7 @@ namespace SeleniumHomeTask.StepDefinitions
             saveJobTitleList = jobTitleList.ClickAdd();
         }
 
-        [Given(@"a job with '([^']*)' exist in table")]
+        [Given(@"a job with (.*) exist in table")]
         public void GivenAJobWithExistInTable(string jobTitle)
         {
             if (!(jobTitleList).GetListOfJobTitlesInTable().Contains(jobTitle))
@@ -38,7 +38,7 @@ namespace SeleniumHomeTask.StepDefinitions
             }
         }
 
-        [When(@"the admin saves new job with valid '([^']*)', '([^']*)' and '([^']*)'")]
+        [When(@"the admin saves new job with valid ([^,]*), (.*) and (.*)")]
         public void WhenTheAdminSavesNewJobWithValidAnd(string jobTitle, string jobDescription, string jobNote)
         {
             saveJobTitleList.EnterJobTitle(jobTitle);
@@ -47,7 +47,7 @@ namespace SeleniumHomeTask.StepDefinitions
             jobTitleList = saveJobTitleList.ClickSave();
         }
 
-        [When(@"the admin delete a job with '([^']*)' through Delete Selected button")]
+        [When(@"the admin delete a job with ([^']*) through Delete Selected button")]
         public void WhenTheAdminDeleteAJobWithThroughDeleteSelectedButton(string jobTitle)
         {
             jobTitleList.CheckCheckboxWithJobTitle(jobTitle);
@@ -55,7 +55,7 @@ namespace SeleniumHomeTask.StepDefinitions
             jobTitleList = jobTitleList.DeleteSelected();
         }
 
-        [Then(@"a job with '([^']*)' and with '([^']*)' exist in table")]
+        [Then(@"a job with (.*) and with (.*) exist in table")]
         public void ThenAJobWithAndWithExistInTable(string jobTitle, string jobDescription)
         {
             Assert.That(RowWithTitleExistInTable(jobTitle));
@@ -63,7 +63,7 @@ namespace SeleniumHomeTask.StepDefinitions
             Assert.That(jobDescription.Contains(jobDescriptionOnSite.TrimStart()));
         }
 
-        [Then(@"row with '([^']*)' don`t exist in table")]
+        [Then(@"row with (.*) don`t exist in table")]
         public void ThenRowWithDonTExistInTable(string jobTitle)
         {
             Assert.That(!RowWithTitleExistInTable(jobTitle));
