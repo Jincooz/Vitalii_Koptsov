4329ee556587372f97b2b4554eaa38db1ef946a0
Vitalii <shaddyreaper@gmail.com>
2023-01-06 16:18:26 +0200

    Create ListFolderApiEndpointResult

---
+++ WebAPIsBascisHomework/WebAPIsBascisHomework/Api/EndpointResult/ListFolderApiEndpointResult.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WebAPIsBascisHomework.Api.EndpointResult
+{
+    internal class ListFolderApiEndpointResult : ApiEndPointResult
+    {
+        public ListFolderApiEndpointResult(HttpResponseMessage httpResponseMessage) : base(httpResponseMessage)
+        {
+        }
+
+        public List<string> FileNames()
+        {
+            string re = "\"name\": \"(.*)\"";
+            Regex regex = new Regex(re);
+            var regexResult = regex.Matches(Body).Cast<Match>().ToList();
+            return regexResult.Select(x => x.Groups[1].Value.ToString()).ToList();
+        }
+    }
+}
---
+++ WebAPIsBascisHomework/WebAPIsBascisHomework/Api/Endpoints/ListFolderApiEndpoint.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPIsBascisHomework.Api.EndpointResult;
+using WebAPIsBascisHomework.Drivers;
+
+namespace WebAPIsBascisHomework.Api
+{
+    internal class ListFolderApiEndpoint : ApiEndpoint
+    {
+        public ListFolderApiEndpoint(Dictionary<string, string> body)
+        {
+            _url = Configuration.Configs["ListFolderApiUrl"];
+            HttpPostDictionary postDictionary = new HttpPostDictionary()
+            {
+                Url = _url
+            };
+            postDictionary.AddDictionary(body);
+            _httpPost = postDictionary;
+        }
+
+        public override ApiEndPointResult Post()
+        {
+            HttpResponseMessage httpResponseMessage = _httpPost.Post();
+            return new ListFolderApiEndpointResult(httpResponseMessage);
+        }
+    }
+}
