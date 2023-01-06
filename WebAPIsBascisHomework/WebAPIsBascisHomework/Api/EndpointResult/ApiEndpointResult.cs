b382bbaf9cd81c8b2acbe360c99c2bd65a3db0bc
Vitalii <shaddyreaper@gmail.com>
2023-01-06 16:16:00 +0200

    Add APIEndpointResult classes for containing POST result

---
+++ WebAPIsBascisHomework/WebAPIsBascisHomework/Api/EndpointResult/ApiEndpointResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPIsBascisHomework.Api
+{
+    internal class ApiEndPointResult
+    {
+        private HttpStatusCode _statusCode;
+        private string _body;
+        public HttpStatusCode StatusCode { get { return _statusCode;} }
+        public string Body { get { return _body; } }
+        public ApiEndPointResult(HttpResponseMessage httpResponseMessage)
+        {
+            _statusCode = httpResponseMessage.StatusCode;
+            _body = httpResponseMessage.Content.ReadAsStringAsync().Result;
+        }
+    }
+}
