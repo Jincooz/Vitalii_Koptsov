1e17c986ebc7ae0452dd73a5538cd1a828ccfbb3
Vitalii <shaddyreaper@gmail.com>
2023-01-06 16:14:03 +0200

    Add Client class for token managing

---
+++ WebAPIsBascisHomework/WebAPIsBascisHomework/Configuration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPIsBascisHomework
+{
+    internal class Configuration
+    {
+        public static Dictionary<string, string> Configs = new Dictionary<string, string>()
+        {
+            {"DropboxAppKey", ""},
+            {"DropboxAppSecret", ""},
+            {"Token", "sl.BWbQXy0E0OsBJr1oJscwCa886oNQEw2kDby11wJwFwucFwmd-qvV2lGXulPe7wl-FHu6uyJ-BL8X1ZB8ncpvb-TuLrvx0wOg1Xhq9bQsUurfO_iGyxOzmF36xxG5Q4WwaTBpyOI"}
+        };
+    }
+}
---
+++ WebAPIsBascisHomework/WebAPIsBascisHomework/Drivers/Client.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace WebAPIsBascisHomework.Drivers
+{
+    internal class Client
+    {
+        private static readonly HttpClient client = new HttpClient();
+        private static string _token = string.Empty;
+        private static Client _client = new Client();
+        public static Client Instance
+        {
+            get 
+            { 
+                return _client;
+            }
+        }
+        private Client() 
+        {
+            GetToken();
+        }
+        private void GetToken()
+        {
+            _token = Configuration.Configs["Token"];
+        }
+        public HttpResponseMessage Post(HttpRequestMessage webRequest)
+        {
+            webRequest.Headers.Authorization = new AuthenticationHeaderValue("Bearer", _token);
+            return client.Send(webRequest);
+        }
+    }
+}
